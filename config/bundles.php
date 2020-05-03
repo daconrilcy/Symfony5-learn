7a8bdd2304d199afcdaaf8b4ac82826797334938
daconrilcy <daconrilcy@hotmail.com>
2020-05-03 16:42:29 +0000

    Add initial set of files

--- config/bundles.php
+++ config/bundles.php
@@ -2,12 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
-=======
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
->>>>>>> Add initial set of files
 ];
